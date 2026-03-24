a90d5570008c3fb991f98c29b9ad276369d9132f
Kyaw Zayar Sein <124577297+kyawzayarseinedu@users.noreply.github.com>
2026-03-24 20:49:55 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,26 +1,16 @@
 import asyncio
-import run as _turbo_core # .so ဖိုင်ကို load လုပ်ခြင်း
+import run 
 
-def start_process():
-    """ 
-    .so ဖိုင်ထဲမှ function များကို စုစည်းပြီး 
-    Process တစ်ခုလုံးကို စတင်ပေးမည့် function
-    """
-    async def main():
+async def start_app():
+    
+    is_verified = await run.verify_activation()
+    if is_verified:
         try:
-            # ၁။ Activation စစ်ဆေးခြင်း
-            if await _turbo_core.verify_activation():
-                print("[+] Activation Verified. Starting Engine...")
-                # ၂။ Main Engine ကို run ခြင်း
-                await _turbo_core.main_engine()
-            else:
-                print("[!] Activation Failed. Access Denied.")
-        except Exception as e:
-            print(f"[!] Error inside process: {e}")
+            
+            await run.main_engine()
+        except KeyboardInterrupt:
+            print("\nStopped by User.")
 
-    try:
-        # Asyncio event loop ကို စတင်ခြင်း
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("\n[!] Stopped by User.")
-        
+if __name__ == "__main__":
+    asyncio.run(start_app())
+    
