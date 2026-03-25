ee9abe51152b889a726469f82e52574dffc47f4a
Kyaw Zayar Sein <124577297+kyawzayarseinedu@users.noreply.github.com>
2026-03-25 12:06:18 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,2 @@
-import asyncio
-import run 
-
-async def start_app():
-    
-    is_verified = await run.verify_activation()
-    if is_verified:
-        try:
-            
-            await run.main_engine()
-        except KeyboardInterrupt:
-            print("\nStopped by User.")
-
-if __name__ == "__main__":
-    asyncio.run(start_app())
-    
+import run
+run.start_process()
